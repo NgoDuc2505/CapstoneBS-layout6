61bdaf62bfe10f74bcf311ad05e13e31dd2143ef
dkav0206 <dkhoa2019vn@gmail.com>
2023-02-07 19:35:35 +1000

    add back to top

--- js/main.js
+++ js/main.js
@@ -13,3 +13,24 @@ function myFunction() {
   document.getElementById("switchButton").onclick = function () {
     document.getElementById("myBody").classList.toggle("dark");
 };
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+// Kéo 20px từ trên đầu xuống => hiển thị nút back to top
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("movetop").style.display = "block";
+  } else {
+    document.getElementById("movetop").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+// Khi click vào nut back to top => lên đầu trang
+function topFunction() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
